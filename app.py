661ab166cba4ee1564ad5f624fe93ee3f03150b1
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-10-22 17:00:34 -0300

    Adding questions resource

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import json
 from flask import Flask
-app = Flask(__name__)
+from flask_cors import CORS
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/questions")
+def Questions():
+    with open('questions.json') as f:
+        data = json.load(f)
+    return json.dumps(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
